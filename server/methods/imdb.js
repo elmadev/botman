6175f22431367aa63da7ea66f34c7f8c68e31681
Roger Andersen <hexjelly@hexjelly.com>
2016-09-15 18:56:03 +0200

    Maybe working import part

--- server/methods/imdb.js
+++ server/methods/imdb.js
@@ -1,6 +1,7 @@
 import {Imdb} from '/lib/collections'
 import moment from 'moment'
 import needle from 'needle'
+import _ from 'lodash'
 
 export default function () {
   Meteor.methods({
@@ -50,7 +51,7 @@ export default function () {
           if (!result) {
             fut.throw(new Meteor.Error(500, `<${response}> is not a valid IMDb rating URL`))
           } else {
-            let url = `http://www.imdb.com/list/export?list_id=ratings&author_id=${result[1]}`
+            let url = `http://akas.imdb.com/list/export?list_id=ratings&author_id=${result[1]}`
             let settings = {
               open_timeout: 60000, // need higher timeout while waiting for imdb export generation
               headers: {
@@ -59,7 +60,7 @@ export default function () {
             }
 
             // fetch rating list
-            needle.get(url, settings, (error, response, result) => {
+            needle.get(url, settings, Meteor.bindEnvironment((error, response, result) => {
               if (error) {
                 fut.throw(new Meteor.Error(500, error))
               } else if (response.statusCode !== 200) {
@@ -75,27 +76,38 @@ export default function () {
                 _.forEach(tmp, line => {
                   line = line.split('","') // don't need edge fields, split this way to remove substring() steps
                   let date = moment(line[2] + '+0000', 'ddd MMM D HH:mm:ss YYYY Z').toDate()
+                  let rating = { discordId: user, date: date, rating: Number(line[8]) }
 
-                  // check if rating differs, if title already exist in rating db
-
-                  // is titleId already in movie list?
-                  // |yes> does userId have rating for it?
-                  // |     |yes> is new rating different?
-                  // |     |     |yes> update it
-                  // |     |     |no>
-                  // |     |no> add rating
-                  // |no> add movie, add rating
-
-                  Imdb.findOne(
-                    { imdbId: line[1] }
-                  )
-
-                  // let tmp = { id: line[1], date: date, title: line[5], type: line[6],
-                  //             rating: line[8], year: line[11], genres: line[12].split(', ') }
+                  let titleDoc = Imdb.findOne({ imdbId: line[1] }, { _id: 1, ratings: 1 })
+                  // does title exist?
+                  if (titleDoc) {
+                    let index = _.findIndex(titleDoc.ratings, { discordId: user })
+                    // has the user rated it already?
+                    if (index > -1) {
+                      // update rating if differs
+                      if (rating.rating !== titleDoc.ratings[index].rating) {
+                        titleDoc.ratings[index] = rating
+                        Imdb.update({ _id: titleDoc._id }, { $set: { ratings: titleDoc.ratings } })
+                      }
+                    } else {
+                      // not rated by user, add rating
+                      Imdb.update({ _id: titleDoc._id }, { $addToSet: { ratings: rating } })
+                    }
+                  } else {
+                    // title does not exist, add it along with rating
+                    Imdb.insert({
+                      imdbId: line[1],
+                      title: line[5],
+                      year: line[11],
+                      genres: line[12].split(', '),
+                      ratings: [rating]
+                    })
+                  }
                 })
+
                 fut['return']({ updated: 0, total: 0 })
               }
-            })
+            }))
           }
         }
       })
