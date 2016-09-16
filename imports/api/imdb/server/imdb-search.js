f02f0b10245f5aa163c003598958e2b95b260fd7
Roger Andersen <hexjelly@hexjelly.com>
2016-09-16 21:35:55 +0200

    Fix poster bug, add avg server rating to !imdb search

--- imports/api/imdb/server/imdb-search.js
+++ imports/api/imdb/server/imdb-search.js
@@ -1,11 +1,13 @@
+import { Meteor } from 'meteor/meteor'
 import { check } from 'meteor/check'
+import { Imdb } from '../imdb'
 import needle from 'needle'
 
 export const imdbSearch = (searchTitle, callback) => {
   check(searchTitle, String)
   let url = `http://www.omdbapi.com/?t=${searchTitle}&y=&plot=short&r=json`
 
-  needle.get(url, (error, response, result) => {
+  needle.get(url, Meteor.bindEnvironment((error, response, result) => {
     if (error) {
       callback(error)
     } else if (response.statusCode !== 200) {
@@ -17,11 +19,40 @@ export const imdbSearch = (searchTitle, callback) => {
       let year = result.Year
       let rating = result.imdbRating
       let url = `http://www.imdb.com/title/${result.imdbID}/`
-      let type = result.Type === 'series' ? ':tv:' : ':movie_camera:'
-      // TODO: fetch server users avg rating
-      let message = { message: `${type} ${title} :date: ${year} :star: ${rating}\n<${url}>`, file: result.Poster }
+      let type
+      switch (result.Type) {
+        case 'series':
+          type = ':tv:'
+          break
+        case 'game':
+          type = ':video_game:'
+          break
+        default:
+          type = ':film_frames:'
+      }
 
+      // server users avg rating
+      let lookup = Imdb.aggregate([
+        { $match: { 'imdbId': result.imdbID } },
+        { $unwind: '$ratings' },
+        { $group: {
+          _id: '$_id',
+          rating: { $avg: '$ratings.rating' },
+          count: { $sum: 1 }
+        }},
+        { $limit: 1 }
+      ])
+
+      let userRating = ''
+      if (lookup.length > 0) {
+        userRating += ` :star2: ${lookup[0].rating} by ${lookup[0].count} user${lookup[0].count > 1 ? 's' : ''}`
+      }
+
+      let message = { message: `${type} ${title} :date: ${year} :star: ${rating}${userRating}\n<${url}>` }
+      if (result.Poster !== 'N/A') {
+        message.file = result.Poster
+      }
       callback(null, message)
     }
-  })
+  }))
 }
