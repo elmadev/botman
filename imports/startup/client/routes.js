587e4e1ab3e2516890b8ccf98b6f3c4feafaa58d
Arturs Demiters <arturs@codecraft.lv>
2016-09-19 19:53:19 +0300

    fix idiot

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -5,7 +5,7 @@ import { Meteor } from 'meteor/meteor'
 import { App } from '../../ui/layouts/app'
 import { Index } from '../../ui/pages/index'
 import { ChatLog } from '../../ui/pages/chatlog'
-import { Musicman } from '../../ui/pages/musicman'
+import { MusicmanHistory } from '../../ui/pages/musicmanhistory'
 
 Meteor.startup(() => {
   render(
@@ -13,7 +13,7 @@ Meteor.startup(() => {
       <Route path='/' component={App}>
         <IndexRoute name='index' component={Index} />
         <Route name='chatlog' path='/chatlog' component={ChatLog} />
-        <Route name='musicman' path='/musicman' component={Musicman} />
+        <Route name='musicman' path='/musicman' component={MusicmanHistory} />
       </Route>
     </Router>,
     document.getElementById('react-root')
--- imports/ui/pages/musicmanhistory.js
+++ imports/ui/pages/musicmanhistory.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 import MusicHistoryGrid from '../containers/musichistory-grid.js'
 
-export const ChatLog = () => (
+export const MusicmanHistory = () => (
   <Row>
     <Col xs={12}>
-      <h4 className='page-header'>musicman history</h4>
+      <h4 className='page-header'>Musicman History</h4>
       <MusicHistoryGrid />
     </Col>
   </Row>
