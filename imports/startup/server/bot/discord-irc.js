ffcd7ea3c6b57e00ed88c97812f991f62ea1a402
Arturs Demiters <arturs@codecraft.lv>
2016-09-23 22:53:35 +0300

    still hopeless wow

--- imports/startup/server/bot/discord-irc.js
+++
@@ -1,6 +0,0 @@
-import discordIRC from 'discord-irc'
-import { Meteor } from 'meteor/meteor'
-
-export default function () {
-  discordIRC(Meteor.settings.discordIrc)
-}
--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,9 +1,9 @@
 import { Meteor } from 'meteor/meteor'
 import './api'
 import bot from './bot'
-import discordIrc from './bot/discord-irc'
+import discordIRC from 'elmadev-discord-irc'
 
 Meteor.startup(() => {
   bot()
-  discordIrc()
+  discordIRC(Meteor.settings.discordIrc)
 })
